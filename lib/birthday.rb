de843bf4666cd0972dc51bcf1f9dc690f72f16e9
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-17 16:43:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,22 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+ birthday_kids = {
+   "Timmy" => 9, 
+   "Sarah" => 6, 
+   "Amanda" => 27
+ }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+    birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
+def age_appropriate_birthday(age)
+  # add your code snippet here!
+    if 
+      birthday_kids.each do |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+    end
+end
